9a6a740a4a262db0ed747375caea8a2424c13c14
sachinthadev <sahansachintha.dev@gmail.com>
2024-07-31 16:56:19 +0530

    created list of product item retrieve from sqlite db.

--- resources/views/admin/product/home.blade.php
+++ resources/views/admin/product/home.blade.php
@@ -31,47 +31,32 @@
                             </caption>
                             <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                 <tr>
-                                    <th scope="col" class="px-6 py-3">Product name</th>
-                                    <th scope="col" class="px-6 py-3">Color</th>
+                                    <th scope="col" class="px-6 py-3">Product no</th>
+                                    <th scope="col" class="px-6 py-3">Product title</th>
                                     <th scope="col" class="px-6 py-3">Category</th>
                                     <th scope="col" class="px-6 py-3">Price</th>
-                                    <th scope="col" class="px-6 py-3"><span class="sr-only">Edit</span></th>
+                                    <th scope="col" class="px-6 py-3">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
+                                @forelse($products as $product )
+
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        Apple MacBook Pro 17"
-                                    </th>
-                                    <td class="px-6 py-4">Silver</td>
-                                    <td class="px-6 py-4">Laptop</td>
-                                    <td class="px-6 py-4">$2999</td>
-                                    <td class="px-6 py-4 text-right">
-                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    <td class="px-6 py-4">{{ $loop->iteration }}</td>
+                                    <td class="px-6 py-4">{{ $product->title }}</td>
+                                    <td class="px-6 py-4">{{ $product->category }}</td>
+                                    <td class="px-6 py-4">{{ $product->price }}</td>
+                                    <td class="px-6 py-4">
+                                        <a href="{{ route('admin/products/edit', '['id'=>$product->id]')}}" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit</a>
+                                        <a href="#" class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Delete</a>
                                     </td>
                                 </tr>
-                                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        Microsoft Surface Pro
-                                    </th>
-                                    <td class="px-6 py-4">White</td>
-                                    <td class="px-6 py-4">Laptop PC</td>
-                                    <td class="px-6 py-4">$1999</td>
-                                    <td class="px-6 py-4 text-right">
-                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                    </td>
+                                @empty
+                                <tr>
+                                    <td class="px-6 py-4" colspan="5">product not found</td>
                                 </tr>
-                                <tr class="bg-white dark:bg-gray-800">
-                                    <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                        Magic Mouse 2
-                                    </th>
-                                    <td class="px-6 py-4">Black</td>
-                                    <td class="px-6 py-4">Accessories</td>
-                                    <td class="px-6 py-4">$99</td>
-                                    <td class="px-6 py-4 text-right">
-                                        <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-                                    </td>
-                                </tr>
+                                @endforelse
+                                
                             </tbody>
                         </table>
                     </div>
