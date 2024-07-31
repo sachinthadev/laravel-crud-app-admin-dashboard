1d9dad6b12783ba5d476a1adb0f396ff23f542e0
sachinthadev <sahansachintha.dev@gmail.com>
2024-07-31 16:13:45 +0530

    created product add function.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,31 +8,38 @@ use Illuminate\Http\Request;
 class ProductController extends Controller
 {
    public function index(){
-    return view('admin.product.home');
+
+      $products=Product::orderBy('id','desc')->get();
+      $total=Product::count();
+      return view('admin.product.home',compact(['products','total']));
+
    }
 
    public function create(){
       return view('admin.product.create');
    }
 
-   public function save(Request $request){
-      $validation = $request->validate([
-         'title'=>'required',
-         'category'=>'required',
-         'price'=>'required',
+   public function save(Request $request)
+   {
+  
+
+      $validation=$request->validate([
+       'title' =>'required',
+       'category'=>'required',
+       'price' =>'required',
       ]);
-
-      $data = Product::create($validation);
+      $data=Product::create($validation);
       if($data){
-         session($key=null,$default = null)->flash('success','Product Add Successfully');
-         return redirect(route('admin/products'));
-      }
-
-      else {
-         session($key=null, $default=null)->flash('error','some problem occure');
-         return redirect(route('admin.products/create'));
+         $request->session()->put('success','Product Add Successfully');
+       return redirect(route('admin/products'));
+      }else{
+         $request->session()->put('error','Some problem occure');
+       return redirect(route('admin.products.create'));
       }
    }
+
+   
+
 }
 
 
--- resources/views/admin/product/home.blade.php
+++ resources/views/admin/product/home.blade.php
@@ -20,7 +20,7 @@
                                         Add Product
                                     </a>
                                     @if(Session::has('success'))
-                                    <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+                                    <div class="text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
                                         <span class="font-medium">{{ Session::get('success') }}</span> 
                                       </div>
                                       @endif
