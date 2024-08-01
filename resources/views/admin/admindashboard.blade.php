35ee3547051ff9825733f3e6648c2e8ffca86dc8
sachinthadev <sahansachintha.dev@gmail.com>
2024-08-02 00:14:01 +0530

    make route from dashboard to the brands.

--- resources/views/admin/admindashboard.blade.php
+++ resources/views/admin/admindashboard.blade.php
@@ -12,6 +12,9 @@
                     <a href="{{ route('admin/products') }}" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                         Products
                     </a>
+                    <a href="{{ route('admin/products/joint') }}" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
+                        Brands
+                    </a>
                 </div>
             </div>
         </div>
--- resources/views/admin/product/home.blade.php
+++ resources/views/admin/product/home.blade.php
@@ -14,6 +14,7 @@
                         <a href="{{ route('admin/products/create') }}" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">
                             Add Product
                         </a>
+                       
                     </div>
                    
 
